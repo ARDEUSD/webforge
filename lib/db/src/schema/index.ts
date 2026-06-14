d6fc2fd45c04bf91ab5af4504cb910a18ae205ea
ARDEUSD <holiyanyadav@outlook.com>
2026-06-14 07:02:11 +0530

    Update index.ts

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -1,4 +1,4 @@
-export * from "./desks";
-export * from "./sessions";
-export * from "./activityLog";
+export { desksTable } from "./desks";
+export { sessionsTable } from "./sessions";
+export { activityLogTable } from "./activityLog";
 export * from "./users";
